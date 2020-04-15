d0073bfa6185124c915d81f88e38d911e2d03293
robertdahmer <robertcontato01@gmail.com>
2020-04-14 22:30:36 -0300

    Mais coisineas

--- Projetos JavaScript/exercicio_legal/script.js
+++ Projetos JavaScript/exercicio_legal/script.js
@@ -1,25 +1,32 @@
 var agora = new Date()
 var horas = agora.getHours()
 var minutos = agora.getMinutes()
+minutos.toString()
+minutos = `0${minutos}`
 var horárioB = `${horas}:${minutos}`
-console.log(horárioB)
-if (horas > 6 && horas < 12){ // manhã
-    var horaDia = window.document.getElementById('horário')
-    horaDia.innerHTML = `Agora são ${horárioB} horas.`
-}
-if (horas < 19){ // tarde
+
+
+if (horas >= 0 && horas < 12){ // manhã
     var horaDia = window.document.getElementById('horário')
     var corpo = window.document.getElementById('corpo')
     var imagem = window.document.getElementById('imagem')
-    horaDia.innerHTML = `Agora são ${horárioB} horas.`
-    imagem.innerHTML = '<img id="imagem"src="fotoafternoon.png" alt="Foto da noite">'
-    corpo.style.background = ''
+    horaDia.innerHTML = `Agora são ${horárioB} horas. Bom dia! :)`
+    imagem.src = 'fotomorning.png'
+    corpo.style.background = 'rgb(217, 91, 42)'
+}
+else if (horas >= 12 && horas < 19){ // tarde
+    var horaDia = window.document.getElementById('horário')
+    var corpo = window.document.getElementById('corpo')
+    var imagem = window.document.getElementById('imagem')
+    horaDia.innerHTML = `Agora são ${horárioB} horas. Boa tarde!`
+    imagem.src = 'fotoafternoon.png'
+    corpo.style.background = 'rgb(0, 166, 233)'
 }
 if (horas >= 19){ // noite
     var horaDia = window.document.getElementById('horário')
     var corpo = window.document.getElementById('corpo')
     corpo.style.background = 'rgb(7, 60, 99)'
     
-    horaDia.innerHTML = `Agora são ${horárioB} horas.`
+    horaDia.innerHTML = `Agora são ${horárioB} horas. Boa Noite!`
 
 }
