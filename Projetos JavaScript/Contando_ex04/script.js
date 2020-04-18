c2ce55e99f45e9ffb92d2ffea805714cc11d5173
robertdahmer <robertcontato01@gmail.com>
2020-04-18 16:16:09 -0300

    Mais estudos

--- Projetos JavaScript/Contando_ex04/script.js
+++ Projetos JavaScript/Contando_ex04/script.js
@@ -4,23 +4,30 @@ var final = window.document.getElementById('final')
 var passo = window.document.getElementById('passo')
 var res = window.document.getElementById('resultado')
 
-if (i == 0 || f == 0 || p == 0){
+if (inicio.value.length == 0 || final.value.length == 0){
+    res.innerHTML = `Impossível contar!`
     window.alert('[ERRO] Consulte os valores e tente novamente!')
-    p = 1
-} else {
+}
+else {
     var i = Number(inicio.value)
     var f = Number(final.value)
     var p = Number(passo.value)
-}
-var c = i
-if (i > f){
-    for (c; c >= f; c -= p){
+    if (p <= 0) {
+        window.alert('PASSO INVÁLIDO! Considerando valor 1')
+        p = 1
+    } 
+    var c = i
+    if (i > f){
+        for (c; c >= f; c -= p){
         res.innerHTML += `${c} \u{1F449}`
-    }
-} else {
-    for (c; c <= f; c += p) {
-    res.innerHTML += `${c} \u{1F449}`
         }
-    }
     res.innerHTML += `\u{1F3C1}`
+    } else {
+        for (c; c <= f; c += p) {
+        res.innerHTML += `${c} \u{1F449}`
+        }
+res.innerHTML += `\u{1F3C1}`
+}
+
+    }
 }
