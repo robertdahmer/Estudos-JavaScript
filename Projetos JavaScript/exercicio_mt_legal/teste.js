8c59b5a978917783ede52572a2068d05aeb60283
robertdahmer <robertcontato01@gmail.com>
2020-05-08 21:40:03 -0300

    Site "cartão" para o dia das mães

---
+++ Projetos JavaScript/exercicio_mt_legal/teste.js
@@ -0,0 +1,3 @@
+let significado = {'mãe': 'Tudo pra mim', 'outras coisas(não-tão-importantes)': 'I eat bread with cement'}
+console.log(significado['mãe'])
+console.log(significado['outras coisas(não-tão-importantes)'])
